646e01a75121c493a4c2439cc505a8336c04ef77
daniel.trone <daniel.trone@breakthroughfuel.com>
2017-03-10 10:08:10 -0600

    squash tile not found errors with mapbox gl js

--- server.js
+++ server.js
@@ -1,55 +1,54 @@
-var express = require("express"),
-  app = express(),
-  MBTiles = require('mbtiles'),
-  p = require("path");
+var express = require('express'),
+    app = express(),
+    MBTiles = require('mbtiles'),
+    p = require('path');
 
 
 // path to the mbtiles; default is the server.js directory
 var tilesDir = __dirname + '/mbtiles';
 
 // Set return header
-function getContentType(t) {
-  var header = {};
+function getContentType (t) {
+    var header = {};
 
-  // CORS
-  header["Access-Control-Allow-Origin"] = "*";
-  header["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept";
+    // CORS
+    header['Access-Control-Allow-Origin'] = '*';
+    header['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
 
-  // Cache
-  //header["Cache-Control"] = "public, max-age=2592000";
+    // Cache
+    // header["Cache-Control"] = "public, max-age=2592000";
 
-  // request specific headers
-  if (t === "png") {
-    header["Content-Type"] = "image/png";
-  }
-  if (t === "jpg") {
-    header["Content-Type"] = "image/jpeg";
-  }
-  if (t === "pbf") {
-    header["Content-Type"] = "application/x-protobuf";
-    header["Content-Encoding"] = "gzip";
-  }
+    // request specific headers
+    if (t === 'png') {
+        header['Content-Type'] = 'image/png';
+    }
+    if (t === 'jpg') {
+        header['Content-Type'] = 'image/jpeg';
+    }
+    if (t === 'pbf') {
+        header['Content-Type'] = 'application/x-protobuf';
+        header['Content-Encoding'] = 'gzip';
+    }
 
-  return header;
+    return header;
 }
 
 // tile cannon
 app.get('/:s/:z/:x/:y.:t', function (req, res) {
-  new MBTiles(p.join(tilesDir, req.params.s + '.mbtiles'), function (err, mbtiles) {
-    mbtiles.getTile(req.params.z, req.params.x, req.params.y, function (err, tile, headers) {
-      if (err) {
-        res.set({
-          "Content-Type": "text/plain"
+    new MBTiles(p.join(tilesDir, req.params.s + '.mbtiles'), function (err, mbtiles) {
+        mbtiles.getTile(req.params.z, req.params.x, req.params.y, function (err, tile, headers) {
+            if (err) {
+                res.set(getContentType('')); // set CORS headers but don't add content type
+                res.sendStatus(204);
+            } else {
+                res.set(getContentType(req.params.t));
+                res.send(tile);
+            }
         });
-        res.status(204).send('Tile rendering error: ' + err + '\n');
-      }
-      else {
-        res.set(getContentType(req.params.t));
-        res.send(tile);
-      }
+        if (err) {
+            console.log('error opening database');
+        }
     });
-    if (err) console.log("error opening database");
-  });
 });
 
 // start up the server
